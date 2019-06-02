8e6b258e3298fb17b2d525bfa989beac192b8d2a
elorenzen <eric.lorenzen@gmail.com>
2019-06-02 05:20:50 -0600

    Added action creator 'fetchUser'

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,18 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+import { async } from 'q';
 
 export const fetchPosts = () => {
     return async (dispatch) => {
         const response = await jsonPlaceholder.get('/posts');
 
         dispatch({ type: 'FETCH_POSTS', payload: response.data });
+    }
+};
+
+export const fetchUser = (id) => {
+    return async (dispatch) => {
+        const response = await jsonPlaceholder.get(`/users/${id}`);
+
+        dispatch({ type: 'FETCH_USER', payload: response.data});
     }
 }
