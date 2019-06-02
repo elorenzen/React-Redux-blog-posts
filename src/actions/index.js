2e8052fe40e2745c09a405c9de68498afb15b8a7
elorenzen <eric.lorenzen@gmail.com>
2019-06-02 06:58:02 -0600

    Used lodash memoize() method to fetch data once per userId

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => {
@@ -5,13 +6,15 @@ export const fetchPosts = () => {
         const response = await jsonPlaceholder.get('/posts');
 
         dispatch({ type: 'FETCH_POSTS', payload: response.data });
-    }
+    };
 };
 
-export const fetchUser = (id) => {
-    return async (dispatch) => {
+export const fetchUser = (id) => (dispatch) => {
+    _fetchUser(id, dispatch);
+};
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
         const response = await jsonPlaceholder.get(`/users/${id}`);
 
         dispatch({ type: 'FETCH_USER', payload: response.data});
-    }
-}
+});
