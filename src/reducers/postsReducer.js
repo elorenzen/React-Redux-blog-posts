482236eb4cd93a352dc0474010f8d47ec2c5957e
elorenzen <eric.lorenzen@gmail.com>
2019-06-01 21:58:57 -0600

    Added switch statements to handle fetchPosts()

---
+++ src/reducers/postsReducer.js
@@ -0,0 +1,10 @@
+// Reducer maintains list of posts fetched from JSON Placeholder API
+
+export default (state = [], action) => {
+    switch (action.type) {
+        case 'FETCH_POSTS':
+            return action.payload;
+        default:
+            return state;
+    }
+};
